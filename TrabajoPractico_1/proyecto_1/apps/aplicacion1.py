43e170fb12c36930d8d56d6b6b7ca062ba74fedc
erni0427 <esteban.erni@ingenieria.uner.edu.ar>
2023-08-15 12:33:15 -0300

    Realizaci{on punto 4,5 y preguntar 2

--- TrabajoPractico_1/proyecto_1/apps/aplicacion1.py
+++ TrabajoPractico_1/proyecto_1/apps/aplicacion1.py
@@ -1,21 +1,56 @@
 # Aplicación secundaria
 from modules.modulo1 import mostrar_lista_peliculas
+from modules.modulo1 import generar_trivia
+from modules.modulo1 import mostrar_y_validar_opciones
+import random
+
+
+
+
+    
+opciones_seleccionadas = []
 
 print("""
-#######################################
-#  Películas: Preguntas y respuestas  #
-#######################################
-Elige una opción
-1 - Mostrar lista de películas.
-2 - ¡Trivia de pelí1cula!
-3 - Mostrar secuencia de opciones seleccionadas previamente.
-4 - Borrar historial de opciones.
-5 - Salir
-"""
+        #######################################
+        #  Películas: Preguntas y respuestas  #
+        #######################################
+        Elige una opción
+        1 - Mostrar lista de películas.
+        2 - ¡Trivia de pelí1cula!
+        3 - Mostrar secuencia de opciones seleccionadas previamente.
+        4 - Borrar historial de opciones.
+        5 - Salir
+        """
 )
 RUTA="./data/"
 DIRECCION=RUTA + "frases_de_peliculas.txt"
-opcion=int(input("Ingrese su opcion: "))
-if opcion==1:
-    for i in mostrar_lista_peliculas(DIRECCION):
-        print(str(i[0])+")",i[1])
+while True:
+    opcion=int(input("Ingrese su opcion: "))
+
+    if opcion==1:
+        for i in mostrar_lista_peliculas(DIRECCION):
+            print(str(i[0])+")",i[1])
+    elif opcion == 2:
+        num_preguntas = int(input("Ingresa el número de preguntas para la trivia (entre 3 y 10): "))
+        if num_preguntas < 3 or num_preguntas > 10:
+            print("El número de preguntas debe estar entre 3 y 10.")
+        else:
+            trivia = generar_trivia(DIRECCION, num_preguntas)
+            for pregunta in trivia:
+                opciones = [pregunta]
+                while len(opciones) < 3:
+                    opcion = random.choice(DIRECCION)
+                    if opcion != pregunta:
+                        opciones.append(opcion)
+                mostrar_y_validar_opciones(pregunta, opciones)
+                opciones_seleccionadas.append(pregunta)  
+    elif opcion == 3:
+            pass
+    elif opcion == 4:
+        opciones_seleccionadas = []
+        print("Historial de opciones borrado.")
+    elif opcion == 5:
+        print("¡Hasta luego!")
+        break
+    else:
+        print("Opción inválida. Por favor, selecciona una opción válida.")
--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,4 +1,5 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
+import random
 lista=[]
 lista_sin_repe=[]
 
@@ -12,3 +13,29 @@ def mostrar_lista_peliculas(archivo_peliculas):
             lista_sin_repe=set(lista) 
     return[(i+1,elemento) for i,elemento in enumerate (sorted(lista_sin_repe))] 
 
+# Generar una trivia
+def generar_trivia(archivo_peliculas, num_preguntas):
+    trivia = random.sample(archivo_peliculas, num_preguntas)
+    return trivia
+
+# Mostrar y validar opciones
+def mostrar_y_validar_opciones(lista_sin_repe, opciones):
+    print("\n" + lista_sin_repe)
+    random.shuffle(opciones)
+    for idx, opcion in enumerate(opciones):
+        print(f"{idx + 1} - {opcion}")
+    
+    respuesta_correcta = opciones.index(lista_sin_repe)
+    while True:
+        try:
+            seleccion = int(input("Selecciona la opción correcta (1/2/3): "))
+            if seleccion < 1 or seleccion > 3:
+                print("Por favor, selecciona una opción válida.")
+            else:
+                if seleccion == respuesta_correcta + 1:
+                    print("¡Correcto! ¡Felicitaciones!\n")
+                else:
+                    print(f"Incorrecto. La respuesta correcta era: {respuesta_correcta + 1} - {lista_sin_repe}\n")
+                break
+        except ValueError:
+            print("Por favor, introduce un número válido.")
