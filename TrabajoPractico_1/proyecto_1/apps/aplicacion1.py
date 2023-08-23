bf28dbb1c491783b0b78186e0f49b94b6a1add70
erni0427 <esteban.erni@ingenieria.uner.edu.ar>
2023-08-23 13:02:54 -0300

    Correccion biblioteca y pleanteo de flask

--- TrabajoPractico_1/proyecto_1/apps/aplicacion1.py
+++ TrabajoPractico_1/proyecto_1/apps/aplicacion1.py
@@ -19,9 +19,11 @@ RUTA="./data/"
 DIRECCION=RUTA + "frases_de_peliculas.txt"
 DIRECCION1=RUTA + "registro_de_opciones_seleccionadas.txt"
 lista_opciones=[]
-opcion=int(input("Ingrese una opciones: "))
+opcion=int(input("Ingrese una opcion: "))
 guardar_opciones(opcion)
-
+with open(DIRECCION,"r",encoding="utf-8") as archi: # utf-8 reconoce caracteres especiales en la Lista
+        lista1=archi.readlines()
+        frases_y_pelis = [(linea.strip().split(';')[0], linea.strip().split(';')[1]) for linea in lista1]  #strip suprime blancos y split va a separar hasta el ;
 while opcion!=5:
     if opcion == 1:
         for i in mostrar_lista_peliculas (DIRECCION):
@@ -30,11 +32,26 @@ while opcion!=5:
         x=int(input("Elija la cantidad de veces que quiere jugar la trivia, debe ser entre 3 y 10: "))
         if x >= 3 and x<=10:
             for i in range (0,x):
-                print(trivia(DIRECCION))
+                lista1=trivia(frases_y_pelis)
+                print ("Adivine a que película pertenece la siguiente frase:", lista1[0])
+                print("Las opciones para elegir son: ")
+                for i,pelicula in enumerate(lista1[2]):
+                    print(i+1,pelicula)
+                opcion=int (input("Elija la opción correcta, ingresando 1, 2 o 3 : "))
+                if opcion-1==lista1[2].index(lista1[1]):
+                    print("¡¡Felicitaciones,la opción elegida es la correcta!!!")
+                elif opcion-1!=lista1[2].index(lista1[1]):
+                    if opcion!=1 and opcion!=2 and opcion!=3:
+                     print("El número ingresado no es una opción posible")
+                    else:
+                        print("La opción es incorrecta :( ")
+            print("\n ¡ Gracias por participar!!!")
         else:
             print("Debe elegir un valor entre 3 y 10")
     elif opcion==3:
-        print (mostrar_opciones_seleccionadas (DIRECCION1))
+        opciones_seleccionadas=mostrar_opciones_seleccionadas (DIRECCION1)
+        print("Las opciones seleccionadas previamente son: ")
+        print(opciones_seleccionadas +"\n")
     elif opcion==4:
         borrar_opciones (DIRECCION1)
         print("El historial se eliminó correctamente")
--- TrabajoPractico_1/proyecto_1/server.py
+++ TrabajoPractico_1/proyecto_1/server.py
@@ -1,48 +1,28 @@
 from flask import render_template, request, redirect, url_for
 from modules.config import app
 from modules.funciones import mostrar_lista_peliculas, trivia, guardar_opciones, mostrar_opciones_seleccionadas, borrar_opciones
+import json
 
 RUTA="./data/"
 DIRECCION=RUTA + "frases_de_peliculas.txt"
 
 
-""""try:
-    cargar_lista_desde_archivo(ARCHIVO, lista_libros)            
+try:
+    mostrar_lista_peliculas(DIRECCION)            
 except FileNotFoundError:
-    with open(ARCHIVO, "w") as archi:
+    with open(DIRECCION, "w") as archi:
         pass
 
-
-@app.route("/")
-def home():
-    esta_vacia = False
-    # opción 1
-    # if request.method == 'POST':
-    #     nombre = request.form['input_nombre']
-    #     autor = request.form['input_autor']
-    #     puntaje = request.form['input_puntaje']
-    #     agregar_libro_a_lista(lista_libros, nombre, autor, puntaje)
-    #     guardar_libro_en_archivo(ARCHIVO, nombre, autor, puntaje)
-
-    if len(lista_libros) == 0:
-        esta_vacia = True
-
-    return render_template('home.html', esta_vacia=esta_vacia, lista_libros=lista_libros)
-
-@app.route("/agregar", methods=["GET", "POST"])
-def agregar():
-    # opción 2
-    if request.method == 'POST':
-        nombre = request.form['input_nombre']
-        autor = request.form['input_autor']
-        puntaje = request.form['input_puntaje']
-        agregar_libro_a_lista(lista_libros, nombre, autor, puntaje)
-        guardar_libro_en_archivo(ARCHIVO, nombre, autor, puntaje)
+@app.route("/", methods=['GET','POST'])
+def trivia():
+    if request.method == 'GET':
+        frase = request.form['input_frase']
+        lista_frases_pelis=trivia(DIRECCION)
 
         return redirect( url_for('home') )
+    return render_template("home.html")
 
-    return render_template('agregar.html')
 
 
-if _name=="main_":
-    app.run(debug=True, host='0.0.0.0')"""
+if __name__=="__main__":
+    app.run(debug=True, host='0.0.0.0')
