c4315043fb57da041ece7d8f7a3d7bb7d19cba56
erni0427 <esteban.erni@ingenieria.uner.edu.ar>
2023-09-05 18:07:23 -0300

    Agregamos que no se repitan las pelis, ahora OK

--- TrabajoPractico_1/proyecto_1/modules/funciones.py
+++ TrabajoPractico_1/proyecto_1/modules/funciones.py
@@ -15,16 +15,39 @@ def mostrar_lista_peliculas(archivo_peliculas):
     lista_sin_repe=set(lista)
     return [(i+1, elemento) for i,elemento in enumerate (sorted(lista_sin_repe))] #elemento no es variable
 
-def trivia (lista_frases_pelis):
+def trivia (lista_frases_pelis,frases_utilizadas=set()):
     """Función que extrae 3 películas y 1 frase al azar y retorna frase y pelicula ganadora ademas de las 3 opciones de peliculas"""
-    op_ganadora=random.choice(lista_frases_pelis) #tupla con frase y pelicula ganadora
-    pelis_no_ganadoras= [p[1] for p in lista_frases_pelis if p != op_ganadora] #Lista de todas las peliculas I= a op ganadora
-    pelis_no_ganadoras1=sorted(set(pelis_no_ganadoras)) #eliminamos las opciones repetidas 
-    opciones=random.sample(pelis_no_ganadoras1, k=2) #Lista de las dos opciones no ganadoras 
-    opciones.append(op_ganadora[1]) #Le agregamos la opcion correcta a la Lista de opciones
-    random.shuffle(opciones) #mezcla Las opciones
-    lista=[op_ganadora[0],op_ganadora[1],opciones]
-    return(lista)
+    # Filtramos las frases que aún no han sido utilizadas
+    frases_disponibles = [p for p in lista_frases_pelis if p not in frases_utilizadas]
+    
+    # Si no hay más frases disponibles, reiniciamos el conjunto de frases utilizadas
+    if not frases_disponibles:
+        frases_utilizadas.clear()
+        frases_disponibles = lista_frases_pelis
+    
+    # Seleccionamos una frase al azar de las disponibles
+    op_ganadora = random.choice(frases_disponibles)
+    
+    # Agregamos la frase utilizada al conjunto
+    frases_utilizadas.add(op_ganadora)
+    
+    # Filtramos las películas no ganadoras
+    pelis_no_ganadoras = [p[1] for p in lista_frases_pelis if p[0] != op_ganadora[0]]
+    
+    # Eliminamos las opciones repetidas
+    pelis_no_ganadoras_unicas = sorted(set(pelis_no_ganadoras))
+    
+    # Elegimos dos opciones no ganadoras al azar
+    opciones = random.sample(pelis_no_ganadoras_unicas, k=2)
+    
+    # Agregamos la opción correcta a la lista de opciones
+    opciones.append(op_ganadora[1])
+    
+    # Mezclamos las opciones
+    random.shuffle(opciones)
+    
+    lista = [op_ganadora[0], op_ganadora[1], opciones]
+    return lista
 
 def guardar_opciones (opciones):
     """Funcion que guarda las opciones seleccionadas por el usuario con el formato %d/%m/%y %H:%M"""
